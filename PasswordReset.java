554ec25ff4579dedd11639a93bdb080c4fe3b535
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-05-06 10:51:32 +0300

    Add files via upload

---
+++ PasswordReset.java
@@ -0,0 +1,51 @@
+package FinalExam;
+
+import java.util.Scanner;
+
+public class PasswordReset {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(scanner.nextLine());
+
+        String line = scanner.nextLine();
+
+        while (!line.equals("Reveal")){
+            String[] data = line.split(":\\|:");
+            String command = data[0];
+            switch (command){
+                case "InsertSpace":
+                    int index = Integer.parseInt(data[1]);
+                    sb.insert(index," ");
+                    break;
+                case "Reverse":
+                    StringBuilder substring = new StringBuilder(data[1]);
+                    if (sb.toString().contains(substring)){
+                       int start = sb.indexOf(String.valueOf(substring.charAt(0)));
+                       int end = start + substring.length() - 1;
+
+                        sb.delete(start,end + 1);
+                        substring.reverse();
+                        sb.append(substring);
+                    }else {
+                        System.out.println("error");
+                        line = scanner.nextLine();
+                        continue;
+                    }
+
+
+                    break;
+                case "ChangeAll":
+                    String occurrence = data[1];
+                    String replaysment = data[2];
+                    String replaysedString = sb.toString().replaceAll(occurrence,replaysment);
+                    sb.setLength(0);
+                    sb.append(replaysedString);
+                    break;
+            }
+
+            System.out.println(sb);
+            line = scanner.nextLine();
+        }
+        System.out.printf("You have a new text message: %s",sb.toString());
+    }
+}
